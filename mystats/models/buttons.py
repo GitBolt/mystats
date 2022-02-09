96ea6ab0e82cb6100c719a62d4c613f22e7f34d0
gitbolt <saabisakhtar@gmail.com>
2022-02-09 23:16:05 +0530

    ☄️ Finish channel creation process

--- mystats/models/buttons.py
+++ mystats/models/buttons.py
@@ -51,13 +51,28 @@ class LobbyGate(disnake.ui.View):
             await self.message.edit(embed=self.embed)
 
             if len(self.lobby.players) == self.lobby.players_required:
-                print("yes")
-                # overwrites = {
-                # self.ctx.guild.default_role: disnake.PermissionOverwrite(view_channel=False),
-                # self.ctx.guild.get_role(): disnake.PermissionOverwrite(read_messages=True)
-                # }
-                # x = self.ctx.guild.create_text_channel(name="Lobby" + len(self.lobby.created_at), overwrites=overwrites)
-                # print(x)
+                overwrites = {
+                    self.ctx.guild.default_role: disnake.PermissionOverwrite(view_channel=False),
+                }
+
+                ch_name = await self.ctx.guild.create_text_channel(
+                    name="Lobby " + self.lobby.created_at.strftime("%H_%M"),
+                    overwrites=overwrites)
+
+                await self.ctx.guild.create_voice_channel(
+                    name="Lobby " + self.lobby.created_at.strftime("%H_%M"),
+                    overwrites=overwrites)
+                    
+            self.embed.title = "The lobby is filled!"
+            self.embed.color = Colours.SUCCESS.value
+            await self.message.edit(embed=self.embed, view=None)
+
+            await self.lobby.channel.send(
+                            "The lobby is filled! Head over to " 
+                            f"{ch_name} text and voice channels. "
+                            " ".join([player.mention for player in self.lobby.players]
+                             )
+            )
 
     @disnake.ui.button(label="Leave", style=disnake.ButtonStyle.red)
     async def leave(
