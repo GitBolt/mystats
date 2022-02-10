9d231778742347af317b58c09584310db4067da4
gitbolt <saabisakhtar@gmail.com>
2022-02-10 23:30:53 +0530

    📝Improved help message

--- mystats/cogs/help.py
+++ mystats/cogs/help.py
@@ -14,22 +14,23 @@ class Help(commands.Cog):
             title="Help is here!",
             color=Colours.DEFAULT.value
         ).add_field(
-            name="!lobby create `<#channel>` `<description>`",
-            value=("Welcome to MYSTATS LOBBY CREATOR.\n\n"
-                   "How to create a LOBBY?\n"
-                   "1. Go to the channel where you want to start the lobby\n"
-                   "2. Enter `!lobby create`\n"
-                   "3. Optionally provide the lobby description. "
+            name="Welcome to MYSTATS LOBBY CREATOR.",
+            value=("\n> __How to create a LOBBY?__\n"
+                   "> 1. Go to the channel where you want to start the lobby\n"
+                   "> 2. Enter `!lobby create`\n"
+                   "> 3. Optionally provide the lobby description. "
                    "For example `!lobby create looking for a quick match`\n"
-                   "3. An embed will be sent with the lobby information and buttons using which people can join\n\n"
-                   "How to join a LOBBY?\n"
-                   "1. Go the game channel to find any open lobby\n"
-                   "2. Click on 'Join' button in the lobby embed\n"
-                   "3. You will be added to the lobby and embed will show your name\n\n"
-                   "How to leave a LOBBY?\n"
-                   "1. Make sure you are in a lobby\n"
-                   "2. Make sure you are not the lobby leader\n"
-                   "3. Find your lobby's embed in your game channel and click on 'Leave' button"
+                   "> 4. An embed will be sent with the lobby information and buttons using which people can join\n\n"
+                   "> __How to join a LOBBY?__\n"
+                   "> 1. Make sure you are not the lobby leader\n"
+                   "> 2. Go the game channel to find any open lobby\n"
+                   "> 3. Click on 'Join' button in the lobby embed\n"
+                   "> 4. You will be added to the lobby and embed will show your name\n\n"
+                   "> __How to leave a LOBBY?__\n"
+                   "> 1. Make sure you are in a lobby\n"
+                   "> 2. Make sure you are not the lobby leader\n"
+                   "> 3. Find your lobby's embed in your game channel and click on 'Leave' button\n"
+                   "> 4. Your slot will become free and your name will be removed from the embed"
                    ),
             inline=False
         ).add_field(
